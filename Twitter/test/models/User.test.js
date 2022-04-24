672970befd0158a0b09f180ff4d4392e72582656
Ajelandro19 <almon190699@gmail.com>
2022-04-23 20:45:16 -0500

    Se agregaron pruebas a los requerimientos

--- Twitter/test/models/User.test.js
+++ Twitter/test/models/User.test.js
@@ -3,7 +3,9 @@ describe("Test para User", () => {
     test("Requerimiento 1: Creacion de User", ()=>{
         const user = new User(1, 'Ajelandro Gonzalez', 'Ajelandro', 'Bio', 'datacreated', 'lastUpdate' )
         expect(user.id).toBe(1)
+        expect(user.username).toBe('Ajelandro Gonzalez' )
         expect(user.name).toBe('Ajelandro')
+        expect(user.bio).toBe('Bio')
     })
 
     test("Requerimiento 2: Fechas en atributos de User",() =>{
@@ -14,10 +16,15 @@ describe("Test para User", () => {
     test("Requerimiento 3: Agregando getters",() =>{
         const user = new User(1,"AjelandroGon", "Ajelandro", "Bio")
         expect(user.getUsername).toBe("AjelandroGon")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
     })
     test("Requerimiento 4: Agregando setters",() =>{
         const user = new User(1,"AjelandroGon", "Ajelandro", "Bio")
         user.setUsername = "A.jelandro"
+        user.setBio = "NewBio"
         expect(user.username).toBe("A.jelandro")
+        expect(user.bio).toBe("NewBio")
     })
 })
